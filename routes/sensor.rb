6b35bf353c6cd3e44c1d28eae38a9c793418971d
Pepe Valdivia <jvaldivia@softwebe.pe>
2017-11-07 13:36:32 -0500

    Avance de pruebas de compoartamiento

--- routes/sensor.rb
+++ routes/sensor.rb
@@ -1,3 +1,7 @@
+# encoding: UTF-8
+# coding: UTF-8
+# -*- coding: UTF-8 -*-
+
 require 'rbejdb'
 
 class MyApp < Sinatra::Base
@@ -6,16 +10,22 @@ class MyApp < Sinatra::Base
         db = EJDB.open("db/quinua", EJDB::DEFAULT_OPEN_MODE)
         db.save("sensores", data)
         db.close
+        'ok'
     end
 
     get '/sensor/listar' do
       db = EJDB.open("db/quinua", EJDB::DEFAULT_OPEN_MODE)
       rs = db.find('sensores')
       i = 0
+      data = ''
       rs.each do |medicion|
-        puts i.to_s + ' - ' + medicion
+        #puts medicion.to_json
         i = i + 1
       end
       db.close #Close the database
+      puts "1 +++++++++++++++++++++++++++++++++++++++++++++++"
+      puts i
+      puts "2 +++++++++++++++++++++++++++++++++++++++++++++++"
+      data
     end
 end
